e57340418846af08c56c8311d876373b98f40198
Bernardo Moya <bernardo_mm@hotmail.com>
2021-01-23 20:45:37 -0600

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitud, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=9517565abc7cdb077b6b8aa94227ff2e&query=' + latitude + ',' + longitud + '&units=m'
+
+    request({ url, json: true }, (error, { body }) => {
+        if(error){
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            console.log(url)
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            const temperature = body.current.temperature
+            const feelsLikeTemp = body.current.feelslike
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out. It feels like ' + feelsLikeTemp + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
